0ce45fb49f50c3a1359c3517f32dbbaaacdf45fb
saul.isern <saul.isern@bluetab.net>
2018-05-30 09:12:42 +0200

    chore: Verify input parameters against entity fileds

--- lib/td_audit/audit/audit.ex
+++ lib/td_audit/audit/audit.ex
@@ -21,14 +21,20 @@ defmodule TdAudit.Audit do
     Repo.all(Event)
   end
 
-  def filter(params) do
+  def filter(params, fields) do
     dynamic = true
     Enum.reduce(Map.keys(params), dynamic, fn (x, acc) ->
-       dynamic([p], field(p, ^String.to_atom(x)) == ^params[x] and ^acc)
+       key_as_atom = String.to_atom(x)
+       case Enum.member?(fields, key_as_atom) do
+         true -> dynamic([p], field(p, ^key_as_atom) == ^params[x] and ^acc)
+         false -> acc
+       end
     end)
   end
+  
   def list_events_by_filter(params) do
-    dynamic = filter(params)
+    fields = Event.__schema__(:fields)
+    dynamic = filter(params, fields)
     Repo.all(from p in Event,
         where: ^dynamic
       )
