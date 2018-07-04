4f23db6852c555436447adaa9de05e138473484d
Nacho <ignacio.hidalgo@bluetab.net>
2018-07-04 15:37:00 +0200

    delete build_centos

--- build_centos/create_secrets_configuration.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sed -i -e "s/username:.*,/username: \"$DB_USER\",/g" ./config/prod.secret.exs
-sed -i -e "s/password:.*,/password: \"$DB_PASSWORD\",/g" ./config/prod.secret.exs
-sed -i -e "s/database:.*,/database: \"$DB_NAME\",/g" ./config/prod.secret.exs
-sed -i -e "s@secret_key:.*@secret_key: \"$GUARDIAN_SECRET_KEY\"@g" ./config/prod.secret.exs
