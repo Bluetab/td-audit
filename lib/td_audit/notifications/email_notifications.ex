2e6341fcb1c5ee8f80448c1a2b7369761df82e02
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-10-18 15:42:22 +0200

    chore: Notifications will be restated

--- lib/td_audit/notifications/email_notifications.ex
+++
@@ -1,8 +0,0 @@
-defmodule TdAudit.Disptach.EmailNotifications do
-  @moduledoc """
-  This module will send notifications regarding the different resources
-  of our system to the envolved users
-  """
-
-
-end
