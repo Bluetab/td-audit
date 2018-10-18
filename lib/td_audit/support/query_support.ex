f5249aae53e7c18b7699895ff0609e7f94207891
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-10-18 17:46:13 +0200

    feat: Add new filter to query json field in data base

--- lib/td_audit/support/query_support.ex
+++ lib/td_audit/support/query_support.ex
@@ -7,12 +7,20 @@ defmodule TdAudit.QuerySupport do
 
   def filter(params, fields) do
     dynamic = true
-    Enum.reduce(Map.keys(params), dynamic, fn (x, acc) ->
-       key_as_atom = String.to_atom(x)
+    Enum.reduce(Map.keys(params), dynamic, fn (key, acc) ->
+       key_as_atom = if is_binary(key), do: String.to_atom(key), else: key
        case Enum.member?(fields, key_as_atom) do
-         true -> dynamic([p], field(p, ^key_as_atom) == ^params[x] and ^acc)
+         true -> filter_by_type(key_as_atom, params[key], acc)
          false -> acc
        end
     end)
+  end
+
+  defp filter_by_type(atom_key, param_value, acc) when is_map(param_value) do
+    dynamic([p], fragment("(?) @> ?::jsonb", field(p, ^atom_key), ^param_value) and ^acc)
+  end
+
+  defp filter_by_type(atom_key, param_value, acc) do
+    dynamic([p], field(p, ^atom_key) == ^param_value and ^acc)
   end
 end
