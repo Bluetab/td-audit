cd9adc37d8ca73ad3976c93e8656a6cd12b0ebff
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-10-23 18:00:32 +0200

    chore: Add new functions in order to treat incoming messages

--- lib/workers/send_event_worker.ex
+++ lib/workers/send_event_worker.ex
@@ -1,10 +1,23 @@
 defmodule TdAudit.SendEventWorker do
+  require Logger
   @moduledoc false
 
   alias TdAudit.Audit
+  alias TdAudit.SearchEventProcessor
+  alias TdAudit.SubscriptionEventProcessor
 
   def perform(event_params) do
+    resp = create_event(event_params)
+    process_event(event_params)
+    resp
+  end
+
+  defp create_event(event_params) do
     Audit.create_event(event_params)
   end
 
+  defp process_event(event_params) do
+    SearchEventProcessor.process_event(event_params)
+    SubscriptionEventProcessor.process_event(event_params)
+  end
 end
