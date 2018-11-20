b4ef3bdb1aed8603e7032fd01ab75cb20f231ee6
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-11-20 10:18:24 +0100

    test: Add needed authentication to run controller tests

--- test/td_audit_web/controllers/configuration_controller_test.exs
+++ test/td_audit_web/controllers/configuration_controller_test.exs
@@ -1,12 +1,24 @@
 defmodule TdAuditWeb.ConfigurationControllerTest do
+  @moduledoc """
+  Test configuration controller
+  """
   use TdAuditWeb.ConnCase
 
   alias TdAudit.NotificationsSystem
   alias TdAudit.NotificationsSystem.Configuration
+  alias TdAuditWeb.ApiServices.MockTdAuthService
 
+  import TdAuditWeb.Authentication, only: :functions
+
+  @admin_user_name "app-admin"
   @create_attrs %{configuration: %{}, event: "some event"}
   @update_attrs %{configuration: %{}, event: "some updated event"}
   @invalid_attrs %{configuration: nil, event: nil}
+
+  setup_all do
+    start_supervised MockTdAuthService
+    :ok
+  end
 
   def fixture(:configuration) do
     {:ok, configuration} = NotificationsSystem.create_configuration(@create_attrs)
@@ -18,6 +30,7 @@ defmodule TdAuditWeb.ConfigurationControllerTest do
   end
 
   describe "index" do
+    @tag authenticated_user: @admin_user_name
     test "lists all notifications_system_configuration", %{conn: conn} do
       conn = get conn, configuration_path(conn, :index)
       assert json_response(conn, 200)["data"] == []
@@ -25,9 +38,12 @@ defmodule TdAuditWeb.ConfigurationControllerTest do
   end
 
   describe "create configuration" do
+    @tag authenticated_user: @admin_user_name
     test "renders configuration when data is valid", %{conn: conn} do
       conn = post conn, configuration_path(conn, :create), configuration: @create_attrs
       assert %{"id" => id} = json_response(conn, 201)["data"]
+
+      conn = recycle_and_put_headers(conn)
 
       conn = get conn, configuration_path(conn, :show, id)
       assert json_response(conn, 200)["data"] == %{
@@ -36,6 +52,7 @@ defmodule TdAuditWeb.ConfigurationControllerTest do
         "event" => "some event"}
     end
 
+    @tag authenticated_user: @admin_user_name
     test "renders errors when data is invalid", %{conn: conn} do
       conn = post conn, configuration_path(conn, :create), configuration: @invalid_attrs
       assert json_response(conn, 422)["errors"] != %{}
@@ -45,9 +62,12 @@ defmodule TdAuditWeb.ConfigurationControllerTest do
   describe "update configuration" do
     setup [:create_configuration]
 
+    @tag authenticated_user: @admin_user_name
     test "renders configuration when data is valid", %{conn: conn, configuration: %Configuration{id: id} = configuration} do
       conn = put conn, configuration_path(conn, :update, configuration), configuration: @update_attrs
       assert %{"id" => ^id} = json_response(conn, 200)["data"]
+
+      conn = recycle_and_put_headers(conn)
 
       conn = get conn, configuration_path(conn, :show, id)
       assert json_response(conn, 200)["data"] == %{
@@ -56,6 +76,7 @@ defmodule TdAuditWeb.ConfigurationControllerTest do
         "event" => "some updated event"}
     end
 
+    @tag authenticated_user: @admin_user_name
     test "renders errors when data is invalid", %{conn: conn, configuration: configuration} do
       conn = put conn, configuration_path(conn, :update, configuration), configuration: @invalid_attrs
       assert json_response(conn, 422)["errors"] != %{}
@@ -65,9 +86,13 @@ defmodule TdAuditWeb.ConfigurationControllerTest do
   describe "delete configuration" do
     setup [:create_configuration]
 
+    @tag authenticated_user: @admin_user_name
     test "deletes chosen configuration", %{conn: conn, configuration: configuration} do
       conn = delete conn, configuration_path(conn, :delete, configuration)
       assert response(conn, 204)
+
+      conn = recycle_and_put_headers(conn)
+
       assert_error_sent 404, fn ->
         get conn, configuration_path(conn, :show, configuration)
       end
