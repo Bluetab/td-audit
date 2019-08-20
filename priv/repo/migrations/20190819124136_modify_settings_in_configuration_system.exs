39a7750b9e1a3a434630942ba722b4bbef07d8a2
saul.isern <saul.isern@bluetab.net>
2019-08-20 13:05:46 +0200

    feat: [TD-1907] migration to add target event to existing configurations

---
+++ priv/repo/migrations/20190819124136_modify_settings_in_configuration_system.exs
@@ -0,0 +1,38 @@
+defmodule TdAudit.Repo.Migrations.ModifySettingsInConfigurationSystem do
+  use Ecto.Migration
+
+  import Ecto.Query
+
+  alias TdAudit.Repo
+
+  def change do
+    from(c in "notifications_system_configuration")
+    |> where([c], c.event == "create_concept_draft")
+    |> select([c], %{id: c.id, settings: c.settings})
+    |> Repo.all()
+    |> Enum.map(&update_settings/1)
+  end
+
+  defp update_settings(%{settings: settings} = params) do
+    generate_subscription = Map.get(settings, "generate_subscription")
+    update_subscription(generate_subscription, params)
+  end
+
+  defp update_subscription(nil, _), do: :ok
+
+  defp update_subscription(generate_subscription, %{id: id, settings: settings}) do
+    case Map.get(generate_subscription, "target_event") do
+      nil ->
+        gs = Map.put(generate_subscription, "target_event", "create_comment")
+        new_settings = Map.put(settings, "generate_subscription", gs)
+
+        from(c in "notifications_system_configuration")
+        |> update([c], set: [settings: ^new_settings])
+        |> where([c], c.id == ^id)
+        |> Repo.update_all([])
+
+      _ ->
+        :ok
+    end
+  end
+end
