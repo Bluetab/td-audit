7d89ae5da1a537d343382e6c99e68b014aa39b0f
Ignacio Gomez <igomezo@ext.laliga.es>
2020-09-22 12:36:23 +0200

    feat: [TD-2939] Limit simultaneous sending of emails

--- lib/td_audit/notifications/dispatcher.ex
+++ lib/td_audit/notifications/dispatcher.ex
@@ -17,7 +17,14 @@ defmodule TdAudit.Notifications.Dispatcher do
   end
 
   def dispatch(periodicity) do
-    GenServer.call(__MODULE__, periodicity)
+    GenServer.cast(__MODULE__, periodicity)
+  end
+
+  def send_email(email) do
+    case Mailer.deliver_now(email, response: true) do
+      {email, {:ok, response}} -> Logger.info("Email sent: Obtained #{response} from server")
+      {email} -> Logger.error("Error sending email to #{email.to}")
+    end
   end
 
   ## GenServer callbacks
@@ -28,17 +35,13 @@ defmodule TdAudit.Notifications.Dispatcher do
   end
 
   @impl GenServer
-  def handle_call(periodicity, _from, state) do
+  def handle_cast(periodicity, state) do
     Logger.debug("Triggering #{periodicity} notifications...")
 
     with {:ok, _} <- Notifications.create(periodicity: periodicity),
          {:ok, %{emails: emails}} when emails != [] <- Notifications.send_pending() do
-      Enum.each(emails, &Mailer.deliver_later/1)
-      {:reply, :ok, state}
-    else
-      {:ok, _} -> {:reply, :ok, state}
-      {:error, failed_operation, _value, _changes} -> {:reply, {:error, failed_operation}, state}
-      error -> {:reply, error, state}
+      Enum.each(emails, &send_email/1)
     end
+    {:noreply, state}
   end
 end
