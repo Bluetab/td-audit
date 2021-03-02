74084012cb1860ad4731926be449eb2310bdbb40
Saul Isern <saul.isern@bluetab.net>
2021-03-02 10:13:42 +0100

    feat: [TD-3063] validate filters

--- lib/td_audit/subscriptions/filters.ex
+++ lib/td_audit/subscriptions/filters.ex
@@ -7,6 +7,10 @@ defmodule TdAudit.Subscriptions.Filters do
 
   import Ecto.Changeset
 
+  alias Ecto.Changeset
+  alias TdCache.TemplateCache
+  alias TdDfLib.Format
+
   @primary_key false
 
   embedded_schema do
@@ -28,20 +32,88 @@ defmodule TdAudit.Subscriptions.Filters do
     changeset
     |> validate_required([:template, :content])
     |> validate_change(:template, &template/2)
-    |> validate_change(:content, &content/2)
+    |> validate_content()
   end
 
-  defp template(:template, template) do
-    case Map.has_key?(template, "id") do
-      true -> []
-      false -> [template: {"expected a map with template id", [validation: :format]}]
+  defp template(:template, %{"id" => id}) do
+    id
+    |> TemplateCache.get()
+    |> case do
+      {:ok, nil} ->
+        [template: {"missing template", [validation: :required]}]
+
+      _ ->
+        []
     end
   end
 
-  defp content(:content, content) do
+  defp template(:template, _tempate),
+    do: [template: {"expected a map with template id", [validation: :format]}]
+
+  defp validate_content(%Changeset{valid?: true} = changeset) do
+    validate_change(changeset, :content, fn _, content -> content(content, changeset) end)
+  end
+
+  defp validate_content(changeset), do: changeset
+
+  defp content(content, %Changeset{valid?: true} = changeset) do
+    content_schema =
+      changeset
+      |> get_field(:template)
+      |> Map.get("id")
+      |> TemplateCache.get()
+      |> case do
+        {:ok, %{} = template} -> template
+        _ -> %{}
+      end
+      |> Map.get(:content, [])
+      |> Format.flatten_content_fields()
+
     case Map.has_key?(content, "name") && Map.has_key?(content, "value") do
-      true -> []
-      false -> [content: {"expected a map with name and value", [validation: :format]}]
+      true ->
+        %{"name" => name, "value" => value} = Map.take(content, ["name", "value"])
+
+        content_schema
+        |> Enum.find(fn %{"name" => field} -> field == name end)
+        |> do_valid_content(name, value)
+
+      false ->
+        [content: {"expected a map with name and value", [validation: :format]}]
     end
   end
+
+  defp do_valid_content(nil, name, _value),
+    do:
+      Keyword.new([{String.to_atom(name), {"missing field on template", [validation: :required]}}])
+
+  defp do_valid_content(%{"values" => %{"fixed" => values}}, name, value) do
+    case value in values do
+      true ->
+        []
+
+      _ ->
+        Keyword.new([
+          {String.to_atom(name),
+           {"missing value in fixed template field", [validation: :required]}}
+        ])
+    end
+  end
+
+  defp do_valid_content(%{"values" => %{"fixed_tuple" => values}}, name, value) do
+    tuple_values = Enum.map(values, &Map.get(&1, "value"))
+
+    case value in tuple_values do
+      true ->
+        []
+
+      _ ->
+        Keyword.new([
+          {String.to_atom(name),
+           {"missing value in fixed tuple template field", [validation: :required]}}
+        ])
+    end
+  end
+
+  defp do_valid_content(_field, name, _value),
+    do: Keyword.new([{String.to_atom(name), {"invalid field format", [validation: :format]}}])
 end
--- test/td_audit/subscriptions/filters_test.exs
+++ test/td_audit/subscriptions/filters_test.exs
@@ -3,10 +3,54 @@ defmodule TdAudit.Subscriptions.FiltersTest do
 
   alias Ecto.Changeset
   alias TdAudit.Subscriptions.Filters
+  alias TdCache.TemplateCache
 
   describe "changeset/2" do
-    test "validates required fields" do
-      params = %{template: %{"id" => 1}}
+    setup do
+      template_id = System.unique_integer([:positive])
+
+      content = [
+        %{
+          "name" => "group",
+          "fields" => [
+            %{
+              name: "foo",
+              type: "string",
+              cardinality: "?",
+              values: %{"fixed" => ["bar"]},
+              subscribable: true
+            },
+            %{
+              name: "xyz",
+              type: "string",
+              cardinality: "?",
+              values: %{"fixed_tuple" => [%{"value" => "foo", "text" => "Foo"}]},
+              subscribable: true
+            }
+          ]
+        }
+      ]
+
+      template = %{
+        id: template_id,
+        name: "foo",
+        label: "label",
+        scope: "test",
+        content: content,
+        updated_at: DateTime.utc_now()
+      }
+
+      TemplateCache.put(template)
+
+      on_exit(fn ->
+        TemplateCache.delete(template_id)
+      end)
+
+      {:ok, [template: template]}
+    end
+
+    test "validates required fields", %{template: %{id: id}} do
+      params = %{template: %{"id" => id}}
 
       assert %{errors: [content: {"can't be blank", [validation: :required]}]} =
                Filters.changeset(params)
@@ -16,15 +60,15 @@ defmodule TdAudit.Subscriptions.FiltersTest do
       assert %{errors: [template: {"can't be blank", [validation: :required]}]} =
                Filters.changeset(params)
 
-      params = %{template: %{"id" => 1}, content: %{"name" => "foo", "value" => "bar"}}
+      params = %{template: %{"id" => id}, content: %{"name" => "foo", "value" => "bar"}}
       assert %Changeset{valid?: true} = Filters.changeset(params)
     end
 
-    test "validates field format" do
-      params = %{template: %{"id" => 1}, content: %{"name" => "foo", "value" => "bar"}}
+    test "validates field format", %{template: %{id: id}} do
+      params = %{template: %{"id" => id}, content: %{"name" => "foo", "value" => "bar"}}
       assert %Changeset{valid?: true} = Filters.changeset(params)
 
-      params = %{template: %{"id" => 1}, content: %{"foo" => "bar"}}
+      params = %{template: %{"id" => id}, content: %{"foo" => "bar"}}
 
       assert %{errors: [content: {"expected a map with name and value", [validation: :format]}]} =
                Filters.changeset(params)
@@ -33,6 +77,56 @@ defmodule TdAudit.Subscriptions.FiltersTest do
 
       assert %{errors: [template: {"expected a map with template id", [validation: :format]}]} =
                Filters.changeset(params)
+    end
+
+    test "validates template fields", %{template: %{id: id}} do
+      params = %{
+        template: %{"id" => System.unique_integer([:positive])},
+        content: %{"name" => "foo", "value" => "bar"}
+      }
+
+      assert %Changeset{
+               valid?: false,
+               errors: [template: {"missing template", [validation: :required]}]
+             } = Filters.changeset(params)
+
+      params = %{
+        template: %{"id" => id},
+        content: %{"name" => "baz", "value" => "bar"}
+      }
+
+      assert %Changeset{
+               valid?: false,
+               errors: [baz: {"missing field on template", [validation: :required]}]
+             } = Filters.changeset(params)
+
+      params = %{
+        template: %{"id" => id},
+        content: %{"name" => "foo", "value" => "xyz"}
+      }
+
+      assert %Changeset{
+               valid?: false,
+               errors: [foo: {"missing value in fixed template field", [validation: :required]}]
+             } = Filters.changeset(params)
+
+      params = %{
+        template: %{"id" => id},
+        content: %{"name" => "xyz", "value" => "bar"}
+      }
+
+      assert %Changeset{
+               valid?: false,
+               errors: [
+                 xyz: {"missing value in fixed tuple template field", [validation: :required]}
+               ]
+             } = Filters.changeset(params)
+
+      params = %{template: %{"id" => id}, content: %{"name" => "foo", "value" => "bar"}}
+      assert %Changeset{valid?: true} = Filters.changeset(params)
+
+      params = %{template: %{"id" => id}, content: %{"name" => "xyz", "value" => "foo"}}
+      assert %Changeset{valid?: true} = Filters.changeset(params)
     end
   end
 end
