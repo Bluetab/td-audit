70e5d3f91d51a13c823dff9a6b8701ae7596cd32
netto <euclydes.young@gmail.com>
2021-09-02 11:44:35 +0200

    chore: update td-df-lib version

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule TdAudit.Mixfile do
       {:bamboo_phoenix, "~> 1.0"},
       {:bamboo_smtp, "~> 4.0.0"},
       {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "4.25.4"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", branch: "feature/td-3973"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "4.27.0"},
       {:gen_stage, "~> 1.0"},
       {:broadway, "~> 0.6.0"},
       {:quantum, "~> 3.0"},
