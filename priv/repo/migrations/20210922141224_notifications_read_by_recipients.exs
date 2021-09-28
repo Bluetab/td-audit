3c937e57a83ccce04f4a42d81e420a7368647a99
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2021-09-28 16:18:32 +0200

    chore: [TD-3396] format documents

--- priv/repo/migrations/20210922141224_notifications_read_by_recipients.exs
+++ priv/repo/migrations/20210922141224_notifications_read_by_recipients.exs
@@ -3,10 +3,11 @@ defmodule TdAudit.Repo.Migrations.NotificationsReadByRecipients do
 
   def change do
     create table(:notifications_read_by_recipients) do
-      add :notification_id, references(:notifications, on_delete: :delete_all)
-      add :recipient_id, :integer
+      add(:notification_id, references(:notifications, on_delete: :delete_all))
+      add(:recipient_id, :integer)
       timestamps(updated_at: false, type: :utc_datetime_usec)
     end
+
     create(
       unique_index(
         :notifications_read_by_recipients,
