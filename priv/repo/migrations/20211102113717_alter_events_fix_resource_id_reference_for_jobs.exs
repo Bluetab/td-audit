9a332cc5508a3d9e054830c08c2421bc621d372a
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2021-11-08 14:59:29 +0100

    chore: [TD-4099] format documents

--- priv/repo/migrations/20211102113717_alter_events_fix_resource_id_reference_for_jobs.exs
+++ priv/repo/migrations/20211102113717_alter_events_fix_resource_id_reference_for_jobs.exs
@@ -2,38 +2,32 @@ defmodule TdAudit.Repo.Migrations.AlterEventsFixResourceIdReferenceForJobs do
   use Ecto.Migration
 
   def up do
-    execute(
-      """
-        update events set
-        payload = events.payload || jsonb_build_object('source_id', events.resource_id),
-        resource_id = cast(events.payload ->> 'job_id' as integer)
-        where events.resource_type = 'jobs'
-      """
-    )
-    execute(
-      """
-        update events
-        set payload = events.payload - 'job_id'
-        where events.resource_type = 'jobs'
-      """
-    )
+    execute("""
+      update events set
+      payload = events.payload || jsonb_build_object('source_id', events.resource_id),
+      resource_id = cast(events.payload ->> 'job_id' as integer)
+      where events.resource_type = 'jobs'
+    """)
+
+    execute("""
+      update events
+      set payload = events.payload - 'job_id'
+      where events.resource_type = 'jobs'
+    """)
   end
 
   def down do
-    execute(
-      """
-        update events set
-        payload = events.payload || jsonb_build_object('job_id', events.resource_id),
-        resource_id = cast(events.payload ->> 'source_id' as integer)
-        where events.resource_type = 'jobs'
-      """
-    )
-    execute(
-      """
-        update events
-        set payload = events.payload - 'source_id'
-        where events.resource_type = 'jobs'
-      """
-    )
+    execute("""
+      update events set
+      payload = events.payload || jsonb_build_object('job_id', events.resource_id),
+      resource_id = cast(events.payload ->> 'source_id' as integer)
+      where events.resource_type = 'jobs'
+    """)
+
+    execute("""
+      update events
+      set payload = events.payload - 'source_id'
+      where events.resource_type = 'jobs'
+    """)
   end
 end
