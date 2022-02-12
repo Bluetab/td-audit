126a54bf76276c890529d87d822da665ee31fc4d
Julio Barriuso <julio.barriuso@bluetab.net>
2022-02-12 17:50:09 +0100

    chore: [TD-4425] credo

--- lib/td_audit/helpers.ex
+++ lib/td_audit/helpers.ex
@@ -1,7 +1,5 @@
 defmodule TdAudit.Helpers do
-  @time_zone Application.get_env :td_audit, :time_zone
-
-  def shift_zone(date_utc_string, time_zone \\ @time_zone) do
+  def shift_zone(date_utc_string, time_zone \\ Application.get_env(:td_audit, :time_zone)) do
       date_utc_string
       |> DateTime.from_iso8601
       |> (fn {:ok, date_utc, _} -> date_utc  end).()
